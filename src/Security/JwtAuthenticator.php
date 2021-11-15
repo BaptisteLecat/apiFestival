81e3824d616f59bdc86135653429d12f058e6558
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-11-15 16:49:52 +0100

    Update JwtAuthenticator.php

--- src/Security/JwtAuthenticator.php
+++ src/Security/JwtAuthenticator.php
@@ -6,6 +6,7 @@ use Firebase\JWT\JWT;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
