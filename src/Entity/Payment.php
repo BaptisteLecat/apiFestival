fb78aab5e2eb2d625e415b3cd06d69f96227b1f3
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2022-01-31 14:35:02 +0100

    Implement payment manager

---
+++ src/Entity/Payment.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PaymentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+/**
+ * @ApiResource(
+ *  normalizationContext={"skip_null_values" = false},
+ *  itemOperations={"get", "put"}
+ * )
+ * @ApiFilter(SearchFilter::class, properties={"session": "exact"}) 
+ * @ORM\Entity(repositoryClass=PaymentRepository::class)
+ */
+class Payment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $datePayment;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="payments")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    public $user;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $session;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $amount;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Event::class, inversedBy="events")
+     */
+    private $event;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDatePayment(): ?\DateTimeInterface
+    {
+        return $this->datePayment;
+    }
+
+    public function setDatePayment(\DateTimeInterface $datePayment): self
+    {
+        $this->datePayment = $datePayment;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getSession(): ?string
+    {
+        return $this->session;
+    }
+
+    public function setSession(?string $session): self
+    {
+        $this->session = $session;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getEvent(): ?Event
+    {
+        return $this->event;
+    }
+
+    public function setEvent(?Event $event): self
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+}
